72bfcc074aec3a49814a57c128b7adfd2912a148
SimLif <517992857@qq.com>
2025-04-06 06:48:11 +0800

    scripts for moe qwen2 vl

--- scripts/v1/qwen2_vl/finetune_moe_med.sh
+++ scripts/v1/qwen2_vl/finetune_moe_med.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
 
 moe_mode="sparse"
-num_experts=4
+num_experts=4096
 top_k_experts=2
 use_residual=False
 router_aux_loss_coef=0.01
 JSON_FOLDER="/mnt/data/haoqiang/workspace/data/medmoe-vqa/3vqa"
+# JSON_FOLDER="/mnt/data/haoqiang/workspace/data/biomed-visual-instructions"
 IMAGE_FOLDER="/mnt/data/haoqiang/workspace/data/medmoe-vqa/images"
+# IMAGE_FOLDER="/mnt/data/haoqiang/workspace/data/pubmedvision/images"
 cd ~/workspace/05-moe-llava
 export WANDB_PROJECT=moe-qwen2vl-med
 export NCCL_P2P_DISABLE=1
 export HF_DATASETS_OFFLINE=1 
-export TRANSFORMERS_OFFLINE=1 
-deepspeed --include=localhost:0,1 moellava/train/train_mem.py \
+export TRANSFORMERS_OFFLINE=1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+deepspeed --include=localhost:3,4 --master_port=29506 moellava/train/train_mem.py \
     --moe_enable True --num_experts ${num_experts} --top_k_experts ${top_k_experts} --capacity_factor 1.5 \
     --moe_mode ${moe_mode} --use_residual ${use_residual} --router_aux_loss_coef ${router_aux_loss_coef} \
     --train_modules mlp.gate_proj mlp.up_proj mlp.down_proj wg \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path /mnt/data/haoqiang/workspace/models/qwen2-vl-2b-instruct \
     --skip_moe_init True \
-    --version qwen \
-    --data_path ${JSON_FOLDER}/train_all_converted.json \
+    --image_min_pixels $((16 * 28 * 28)) \
+    --image_max_pixels $((576 * 28 * 28)) \
+    --mone_enable True \
+    --mone_expert_type "embedding_expert" \
+    --mone_gate_type "token_gating" \
+    --mone_r 2 \
+    --mone_num_heads 8 \
+    --version med-moe \
+    --data_path ${JSON_FOLDER}//mnt/data/haoqiang/workspace/data/med-k-nlp/zh-huatuo-sft-50k.json.json \
     --image_folder ${IMAGE_FOLDER} \
-    --image_tower /mnt/data/haoqiang/workspace/models/clip-vit-large-patch14-336 \
-    --image_projector_type mlp2x_gelu \
+    --image_tower /mnt/data/haoqiang/workspace/models/qwen2-vl-2b-instruct \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/moe-llava-qwen-1.8b-4e-1epoch\
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 8 \
+    --output_dir ./checkpoints/qwen2-vl-2b-instruct-${num_experts}e${top_k_experts}-med-nano-ee-5epoch\
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
@@ -45,10 +54,15 @@ deepspeed --include=localhost:0,1 moellava/train/train_mem.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 2048 \
+    --model_max_length 1024 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 8 \
     --lazy_preprocess True \
     --report_to wandb \
-    --run_name "test-3vqa" \
+    --run_name "3vqa-nano" \
     --cache_dir "./cache_dir"
+
+
+# --output_dir ./checkpoints/qwen2-vl-2b-instruct-${num_experts}e${top_k_experts}-ada-nano-test\
+# --output_dir ./checkpoints/qwen2-vl-2b-instruct-${num_experts}e8x${top_k_experts}-med-nano-ee-smi-5e\
+    # --report_to wandb \
