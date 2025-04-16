28c02daac778eae03ceda2c746ac46965b646d67
SimLif <517992857@qq.com>
2025-04-17 02:07:16 +0800

    在 train_mem.py 中添加随机种子设置以确保训练过程的可重复性

--- moellava/train/train_mem.py
+++ moellava/train/train_mem.py
@@ -3,6 +3,11 @@
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 
 # Need to call this before importing transformers.
+import random
+import numpy as np
+import torch
+from transformers import set_seed
+
 from moellava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 
 replace_llama_attn_with_flash_attn()
@@ -10,4 +15,11 @@ replace_llama_attn_with_flash_attn()
 from moellava.train.train import train
 
 if __name__ == "__main__":
+    seed = 42
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    set_seed(seed)
+    
     train()
